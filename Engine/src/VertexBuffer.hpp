6bb68ac7f8045a53132f4872648adf61f304133b
DOXECEES <90423472+DOXECEES@users.noreply.github.com>
2023-06-08 22:24:13 +0300

    Add vertex buffer class

---
+++ Engine/src/VertexBuffer.hpp
@@ -0,0 +1,16 @@
+
+#include <glfw/glfw3.h>
+#include <glad/glad.h>
+
+
+class VertexBuffer 
+{
+public:
+
+    VertexBuffer();
+    ~VertexBuffer();
+
+private:
+    GLuint VBO;
+
+}
--- Engine/src/main.cpp
+++ Engine/src/main.cpp
@@ -1,18 +1,39 @@
+
 #include <glad/glad.h>
 #include <GLFW/glfw3.h>
 
 #include <iostream>
+
+#include "Window.hpp"
+
+void error_callback(int error, const char* description)
+{
+	std::cout << "[ERROR CODE]: " << error << std::endl <<
+				 "[DESCRIPTION] " << description << std::endl;
+} 
+
+
 int main()
 {
-	if (!glfwInit()) {
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+
+
+	glfwSetErrorCallback(error_callback);
+
+	if (!glfwInit()) 
+	{
 		std::cout << "GLFW initialization failed!" << std::endl;
 		exit(EXIT_FAILURE);
 	}
-	GLFWwindow* window;
-	window = glfwCreateWindow(640, 480, "dsa", nullptr, nullptr);
-	while (true)
+
+	Render::Window *wnd = new Render::Window(1920,1080,"ass");
+	
+	while (wnd->Render())
 	{
+				
 
 	}
+
 	glfwTerminate();
 }
