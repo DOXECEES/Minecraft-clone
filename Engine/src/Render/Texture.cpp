4b2d69dfe5fb590422e1530ef3caee09f59588ef
DOXECEES <90423472+DOXECEES@users.noreply.github.com>
2024-02-06 03:56:46 +0300

    fix: ripple

--- Engine/src/Render/Texture.cpp
+++ Engine/src/Render/Texture.cpp
@@ -20,9 +20,10 @@ void Renderer::Texture::SetData(const std::vector<unsigned char> &data, const GL
 {
     glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_REPEAT);
     glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_REPEAT);
-    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_NEAREST);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_NEAREST_MIPMAP_LINEAR);
     glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_NEAREST);
     glTexImage2D(GL_TEXTURE_2D, 0, GL_RGBA, width, height, 0, GL_RGBA, GL_UNSIGNED_BYTE, data.data());
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAX_LEVEL, 4);
     glGenerateMipmap(GL_TEXTURE_2D);
     UnBind();
 }
